73e684e00ce4c59bfefed3e95a42cf19fc89958f
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2023-12-31 18:51:20 +0700

    2

--- src/blockchain/index.ts
+++ src/blockchain/index.ts
@@ -1,3 +1,13 @@
+import { AggregatorContract, FactoryContract, PoolContract } from "./contracts"
+
 export * from "./contracts"
 export * from "./query"
 export * from "./metamask"
+
+export const blockchain = {
+    contracts: {
+        Aggregator: AggregatorContract,
+        Factory: FactoryContract,
+        PoolContract: PoolContract,
+    }
+}

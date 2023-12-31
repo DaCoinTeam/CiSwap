32a0bff85394f8c998c4b654b2a21ac6bb5b0479
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2023-12-31 07:08:16 +0700

    1231

--- src/api/next/smart-router/index.ts
+++ src/api/next/smart-router/index.ts
@@ -5,30 +5,33 @@ import { Address, Bytes } from "web3"
 const ROUTER_URL = "/services/smart-router"
 
 export const smartRouterService = {
-    get : async (
+    get: async (
         chainId: ChainId,
         amount: bigint,
         tokenIn: Address,
         tokenOut: Address,
         exactInput: boolean
-    ) => {
-        const params = new URLSearchParams()
-        params.set("chainId", chainId.toString())
-        params.set("amount", amount.toString())
-        params.set("tokenIn", tokenIn)
-        params.set("tokenOut", tokenOut)
-        params.set("exactInput", exactInput.toString())
-        return axios.get(`${ROUTER_URL}?${params.toString()}`)
+    ): Promise<BestRouteResult | null> => {
+        try {
+            const params = new URLSearchParams()
+            params.set("chainId", chainId.toString())
+            params.set("amount", amount.toString())
+            params.set("tokenIn", tokenIn)
+            params.set("tokenOut", tokenOut)
+            params.set("exactInput", exactInput.toString())
+            return await axios.get(`${ROUTER_URL}?${params.toString()}`) as BestRouteResult
+        } catch (ex) {
+            return null
+        }
     },
 }
 
 export default smartRouterService
 
-
-export type Step = (Address | number)
+export type Step = Address | number;
 
 export interface BestRouteResult {
-    amount: bigint;
-    path: Step[];
-    bytes: Bytes
-  }
+  amount: bigint;
+  path: Step[];
+  bytes: Bytes;
+}
